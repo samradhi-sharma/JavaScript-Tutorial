50a559cad9ec7cf95dff1ac7b098974e0c0778c1
Samradhi <samradhisharma36@gmail.com>
2024-10-31 07:26:01 +0530

    pure function and index file added

---
+++ Functions/pureFunction.js
@@ -0,0 +1,15 @@
+// Pure Function 
+
+function calculate(num1, num2){
+    return num1*num2;
+}
+console.log(calculate(4,6));
+
+
+
+
+// Impure Functions
+const discount = 25;
+function calDis(price){
+    return price - discount;
+}
