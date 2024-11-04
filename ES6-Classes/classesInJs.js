be3ce8c2078dc2ed7bbf18eca860c600f3564491
Samradhi <samradhisharma36@gmail.com>
2024-11-04 09:20:30 +0530

    constructor-function and classes

--- ES6-Classes/classesInJs.js
+++ ES6-Classes/classesInJs.js
@@ -8,6 +8,20 @@ function Vehicle(name, color, wheels){
             It has ${this.wheels} wheels`);
     };
 }
+
+/**OR : this constructor function can also be written as */
+function Vehicle(name, color, wheels){
+    this.name = name;
+    this.color = color;
+    this.wheels = wheels;}
+    Vehicle.prototype.getDetails = function(){
+        console.log( `The ${this.name} is ${this.color} in color.
+            It has ${this.wheels} wheels`);
+    };
+
+    
+
+// this is how we define class
 class VehicleCl{
     // properties-> writing these properties is optional 
     name;
@@ -30,3 +44,6 @@ class VehicleCl{
 
 const veh1 = new VehicleCl('Bike', 'Red', 2);
 console.log(veh1);
+
+// Vehicle is a constructor function used to create instances of vehicles.
+// VehicleCl is a class that essentially serves the same purpose but is written in a more modern and concise syntax introduced in ES6 (ES2015).
